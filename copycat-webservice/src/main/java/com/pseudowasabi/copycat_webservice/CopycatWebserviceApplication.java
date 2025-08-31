a0366b6e1ddc337d843696debab8b276f54d9ca4
pseudowasabi <pseudowasabi@kakao.com>
2025-08-31 22:06:25 +0900

    replace as-is module with new module created by spring initializr

---
+++ copycat-webservice/src/main/java/com/pseudowasabi/copycat_webservice/CopycatWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pseudowasabi.copycat_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopycatWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopycatWebserviceApplication.class, args);
+	}
+
+}
