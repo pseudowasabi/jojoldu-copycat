f083d8319176da063c8d1dd184d9983a9bece2e4
pseudowasabi <pseudowasabi@kakao.com>
2025-09-01 22:27:34 +0900

    add an entity named Posts

---
+++ copycat-webservice/src/main/java/com/pseudowasabi/copycat_webservice/domain/posts/Posts.java
@@ -0,0 +1,34 @@
+package com.pseudowasabi.copycat_webservice.domain.posts;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
+
+// NOTE
+// DO NOT CREATE ANY @Setter METHODS IN ENTITY CLASS
